ce255f6b2730f3466f368f71101725d6903239d7
Rihab Oueslati <rihaboueslati@takiacademyteam.com>
2023-08-30 21:50:37 +0100

    try if git connected

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
 import Cover from "@/components/Cover";
 import Footer from "@/layout/Footer";
-import Header from "@/layout/Header";
+
 import Service from "@/layout/Service";
 
 export default function Home() {
   return (
     <main>
-      <Header />
+  
       <Cover />
       <Service />
       <Footer />
